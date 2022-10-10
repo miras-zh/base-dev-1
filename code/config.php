877561b3194dd348c06995a83b37f334eb2f2522
mikos.zh8 <mikos.zh8@gmail.com>
2022-10-10 14:48:41 +0600

    add config

--- code/config.php
+++ code/config.php
@@ -2,4 +2,14 @@
 
 defined('VG_ACCESS') or die('Access denied');
 
+const SITE_URL = 'http://base-dev.local';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'bd';
+
+
+
 echo 'Heelo';
---
+++ code/core/base/settings/internal_settings.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('VG_ACCESS') or die('Access denied');
+
+const TEMPLATE = 'templates/default/';
+const ADMIN_TEMPLATES = 'core/admin/views/';
+
+const COOKIE_VERSION = '1.0.0';
+const CRYPT_KEY = '';
+const COOKIE_TIME = 60;
+const BLOCK_TIME = 3;
+
+const QTY = 8;
+const QTY_LINK = 3;
+
+const ADMIN_CSS_JS = [
+    'styles'=>[],
+    'scripts'=>[]
+];
