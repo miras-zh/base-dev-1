45bf683d2f5c6eea54a56e13ed25eeb7df055766
miras <mikos.zh8@gmail.com>
2023-07-19 16:14:35 +0600

    fixL layout - actual link dash

--- code/templates/layout.php
+++ code/templates/layout.php
@@ -18,12 +18,12 @@ $user = $userModel->read($_SESSION['user_id']);
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
-    <title>Dashboard | CRM </title>
+    <title>Kazcic CRM </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="crm_mikos" name="author"/>
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href="/templates/assets/images/favicon.ico">
+    <link rel="shortcut icon" href="/templates/assets/images/logo_kazcic.ico">
 
     <!-- Daterangepicker css -->
     <link rel="stylesheet" href="/templates/assets/vendor/daterangepicker/daterangepicker.css">
